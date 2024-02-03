ea2a6f361fafa5d7a7f98b97e93af38c5c7f8501
정문영 <jmuny@jeongmun-yeong-ui-MacBookAir.local>
2024-02-04 04:55:10 +0900

    기본 환경 세팅

---
+++ src/main/java/spectacle/specto/SpectoApplication.java
@@ -0,0 +1,13 @@
+package spectacle.specto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpectoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpectoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/spectacle/specto/SpectoApplicationTests.java
@@ -0,0 +1,13 @@
+package spectacle.specto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpectoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
