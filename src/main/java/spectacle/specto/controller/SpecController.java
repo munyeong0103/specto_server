c7f72d0edec84f0dbe1f8d509f52536b6e61c536
juuns <juuns@ajou.ac.kr>
2024-03-26 03:56:43 +0900

    feat: implement getSpecBy recent & oldest with pageable (#5)

--- src/main/java/spectacle/specto/controller/SpecController.java
+++ src/main/java/spectacle/specto/controller/SpecController.java
@@ -1,46 +1,54 @@
 package spectacle.specto.controller;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import spectacle.specto.dto.SpecPostReq;
+import spectacle.specto.domain.enumType.Category;
+import spectacle.specto.dto.specDto.req.SpecPostReq;
+import spectacle.specto.dto.specDto.res.SpecRes;
+import spectacle.specto.service.SpecService;
+
+import java.util.List;
 
 @RestController
-@RequestMapping("/spec")
+@RequiredArgsConstructor
+@RequestMapping("/api/v1/spec")
 public class SpecController {
+
+    private final SpecService specService;
 
     // 스펙 조회 - 최근 등록순 정렬
     @GetMapping("/recent")
-    public ResponseEntity<?> getSpecByRecent(@RequestParam String category, Pageable pageable) {
-
-        return ResponseEntity.ok(HttpStatus.OK);
+    public ResponseEntity<?> getSpecByRecent(@RequestParam Category category, Pageable pageable) {
+        List<SpecRes> specByRecent = specService.getSpecByRecent(category, pageable);
+        return ResponseEntity.ok(specByRecent);
     }
 
     // 스펙 조회 - 오래된순 정렬
     @GetMapping("/oldest")
-    public ResponseEntity<?> getSpecByOldest(@RequestParam String category, Pageable pageable) {
-
-        return ResponseEntity.ok(HttpStatus.OK);
+    public ResponseEntity<?> getSpecByOldest(@RequestParam Category category, Pageable pageable) {
+        List<SpecRes> specByOldest = specService.getSpecByOldest(category, pageable);
+        return ResponseEntity.ok(specByOldest);
     }
 
     // 스펙 조회 - 조회순 정렬
     @GetMapping("/most-reviewed")
-    public ResponseEntity<?> getSpecByMostViewed(@RequestParam String category, Pageable pageable) {
+    public ResponseEntity<?> getSpecByMostViewed(@RequestParam Category category, Pageable pageable) {
 
         return ResponseEntity.ok(HttpStatus.OK);
     }
 
     // 스펙 상세 조회
     @GetMapping("/{specId}")
-    public ResponseEntity<?> getSpecDetail(@PathVariable Long specId, @RequestParam String category, Pageable pageable) {
+    public ResponseEntity<?> getSpecDetail(@PathVariable Long specId, @RequestParam Category category, Pageable pageable) {
 
         return ResponseEntity.ok(HttpStatus.OK);
     }
 
-    @PostMapping("/")
+    @PostMapping("")
     public ResponseEntity<?> createSpec(@RequestBody SpecPostReq specPostReq) {
-
         return ResponseEntity.ok(HttpStatus.OK);
     }
 
--- src/main/java/spectacle/specto/service/SpecServiceImpl.java
+++ src/main/java/spectacle/specto/service/SpecServiceImpl.java
@@ -1,14 +1,14 @@
 package spectacle.specto.service;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
-import spectacle.specto.domain.Spec;
-import spectacle.specto.dto.SpecDetailRes;
-import spectacle.specto.dto.SpecPostReq;
-import spectacle.specto.dto.SpecRes;
-import spectacle.specto.repository.SpecRepository;
+import spectacle.specto.domain.*;
+import spectacle.specto.domain.enumType.Category;
+import spectacle.specto.dto.specDto.req.*;
+import spectacle.specto.dto.specDto.res.SpecDetailRes;
+import spectacle.specto.dto.specDto.res.SpecRes;
+import spectacle.specto.repository.*;
 
 import java.util.List;
 
@@ -17,29 +17,49 @@ import java.util.List;
 public class SpecServiceImpl implements SpecService{
 
     private final SpecRepository specRepository;
+    private final ActivityRepository activityRepository;
+    private final CertificationRepository certificationRepository;
+    private final ContestRepository contestRepository;
+    private final InternshipRepository internshipRepository;
+    private final ProjectRepository projectRepository;
+
+    private final UserRepository userRepository;
 
     @Override
-    public List<SpecRes> getSpecByRecent(String category, Pageable pageable) {
+    public List<SpecRes> getSpecByRecent(Category category, Pageable pageable) {
+        List<Spec> specList = specRepository.findByCategoryOrderByStartDateDesc(category, pageable);
+        return specList.stream().map(this::specToSpecRes).toList();
+    }
+
+    @Override
+    public List<SpecRes> getSpecByOldest(Category category, Pageable pageable) {
+        List<Spec> specList = specRepository.findByCategoryOrderByStartDateAsc(category, pageable);
+        return specList.stream().map(this::specToSpecRes).toList();
+    }
+
+    @Override
+    public List<SpecRes> getSpecByMostViewed(Category category, Pageable pageable) {
         return null;
     }
 
     @Override
-    public List<SpecRes> getSpecByOldest(String category, Pageable pageable) {
-        return null;
-    }
-
-    @Override
-    public List<SpecRes> getSpecByMostViewed(String category, Pageable pageable) {
-        return null;
-    }
-
-    @Override
-    public SpecDetailRes getSpecDetail(Long specId, String category, Pageable pageable) {
+    public SpecDetailRes getSpecDetail(Long specId, Category category, Pageable pageable) {
         return null;
     }
 
     @Override
     public Long createSpec(SpecPostReq specPostReq) {
         return null;
+    }
+
+    private SpecRes specToSpecRes(Spec spec) {
+        return SpecRes.builder()
+                .specId(spec.getId())
+                .name(spec.getName())
+                .category(spec.getCategory().toString())
+                .startDate(spec.getStartDate())
+                .endDate(spec.getEndDate())
+                .completed(spec.isCompleted())
+                .build();
     }
 }
